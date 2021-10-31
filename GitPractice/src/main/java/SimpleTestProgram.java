564f7546775b70a339795a067173c24450bcacd7
Mahadev <Mahadev@DESKTOP-DJBPQ3I>
2021-10-31 19:23:58 +0530

    commit on develop branch

--- GitPractice/src/main/java/SimpleTestProgram.java
+++ GitPractice/src/main/java/SimpleTestProgram.java
@@ -1,16 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class SimpleTestProgram {
 
 	public static void main(String[] args) {
 
 		try {
+			int sum =0;
 			ExecutorService service = Executors.newFixedThreadPool(10);
-			
-			for(int i=0; i<100; i++) {
-				service.execute(new Task());
+//			ExecutorService service = Executors.newCachedThreadPool();
+			List<Future<Integer>> futureList = new ArrayList<Future<Integer>>();
+			for(int i=0; i<10; i++) {
+				Future<Integer> value = service.submit(new Task1());
+				futureList.add(value);
 			}
+			
+			for(Future<Integer> value: futureList) {
+				sum = sum + value.get();
+			}
+			System.out.println("Total Sum: "+sum);
 			System.out.println("Main Thread: "+Thread.currentThread().getName());
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -23,6 +36,15 @@ public class SimpleTestProgram {
 			System.out.println("Current Thread: "+Thread.currentThread().getName());
 			
 		}
+	}
+	
+	static class Task1 implements Callable<Integer>{
+
+		public Integer call() throws Exception {
+			return new Random().nextInt();
+		}
+
 		
 	}
+	
 }
