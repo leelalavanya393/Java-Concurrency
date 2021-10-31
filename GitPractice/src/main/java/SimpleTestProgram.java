d54d2176c6184778a780855e2598b45163bf00b7
Mahadev <Mahadev@DESKTOP-DJBPQ3I>
2021-10-31 19:00:21 +0530

    Initial Commit

---
+++ GitPractice/src/main/java/SimpleTestProgram.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleTestProgram {
+
+	public static void main(String[] args) {
+
+		try {
+			ExecutorService service = Executors.newFixedThreadPool(10);
+			
+			for(int i=0; i<100; i++) {
+				service.execute(new Task());
+			}
+			System.out.println("Main Thread: "+Thread.currentThread().getName());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	static class Task implements Runnable{
+
+		public void run() {
+			System.out.println("Current Thread: "+Thread.currentThread().getName());
+			
+		}
+		
+	}
+}
